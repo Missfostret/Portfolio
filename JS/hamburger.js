0885e5ee9ed8d4e4f38be2d0602d56e053137262
Missfostret <67271654+Missfostret@users.noreply.github.com>
2025-11-27 15:34:31 +0100

    Add initial styles and structure for the portfolio website with responsive hamburger menu

---
+++ JS/hamburger.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    hamburger.addEventListener("click", () => {
+        hamburger.classList.toggle("open");
+    });
+});
